8473777c74ac26db77c4b9a3760b2a0678ef8d32
jhonEdison1 <47945021+jhonEdison1@users.noreply.github.com>
2023-08-16 18:47:20 -0500

    cambios

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix("eyedmx");
   app.useGlobalPipes(new ValidationPipe(
     {
